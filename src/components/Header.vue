e9c061cecd3a0875c12e479fa06c277e8f2e05d1
lindabettini <linda.bettini@hotmail.it>
2022-02-08 19:32:21 +0100

    add dinamincs list items

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,8 +2,14 @@
 	<header>
 		<div class="container">
 			<nav>
-				<div>ciao</div>
-				<div>ciao</div>
+				<figure>
+					<img src="../img/dc-logo.png" alt="DC logo" />
+				</figure>
+				<ul>
+					<li v-for="(link, index) in links" :key="index">
+						<a :href="link.url">{{ link.text }}</a>
+					</li>
+				</ul>
 			</nav>
 		</div>
 	</header>
@@ -12,21 +18,51 @@
 <script>
 export default {
 	name: "Header",
+	data() {
+		return {
+			links: [
+				{ text: "Characters", url: "#", active: false },
+				{ text: "Comics", url: "#", active: false },
+				{ text: "Movies", url: "#", active: false },
+				{ text: "Tv", url: "#", active: false },
+				{ text: "Games", url: "#", active: false },
+				{ text: "Collectibles", url: "#", active: false },
+				{ text: "Video", url: "#", active: false },
+				{ text: "Fans", url: "#", active: false },
+				{ text: "News", url: "#", active: false },
+				{ text: "Shop", url: "#", active: false },
+			],
+		};
+	},
 };
 </script>
 
 <style scoped lang="scss">
 header {
 	height: 80px;
-	background-color: pink;
+	background-color: white;
+	.container {
+		height: 100%;
+		nav {
+			height: 100%;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+		}
+	}
 }
-.container {
-	height: 100%;
+
+img {
+	width: 60px;
 }
-nav {
-	height: 100%;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
+
+ul {
+	list-style-type: none;
+	li {
+		display: inline-block;
+	}
+	li a {
+		padding: 10px;
+	}
 }
 </style>
