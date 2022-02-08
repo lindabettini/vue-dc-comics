29eb7fcc618b75e8efa0fb869f35918bffa27add
lindabettini <linda.bettini@hotmail.it>
2022-02-08 19:48:28 +0100

    add content section

---
+++ src/components/Content.vue
@@ -0,0 +1,29 @@
+<template>
+	<section id="content">
+		<div class="container">
+			<h3>Content goes here</h3>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	name: "Content",
+};
+</script>
+
+<style scoped lang="scss">
+#content {
+	height: 80px;
+	background-color: black;
+	.container {
+		display: flex;
+		height: 100%;
+		align-items: center;
+	}
+}
+
+h3 {
+	color: white;
+}
+</style>
